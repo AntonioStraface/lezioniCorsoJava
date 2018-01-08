9d18b97fb0888901247c8a5f5c892a7ad030b291
antonio\strafacea <antonio.straface@scai-consulting.grupposcai.it>
2018-01-08 14:39:42 +0100

    esercizi matrici

---
+++ fondamentidiinformatica/matrici/Main.java
@@ -0,0 +1,12 @@
+package fondamentidiinformatica.matrici;
+
+class Main {
+
+	public static void main(String[] args) {
+		int a[][] = new int[][] {{4,5,6},{7,8,9},{10,11,12}};
+		System.out.println(Matrice.sommadiagonale(a));
+		System.out.println(Matrice.fakeToString(a));
+		System.out.println(Matrice.fakeToString(Matrice.trasposta(a)));
+	}
+
+}
