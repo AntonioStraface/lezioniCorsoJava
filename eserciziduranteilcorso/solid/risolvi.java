bcddb1139294fc0ed22bd57e43f203a209fa092c
DESKTOP-3CV1TKT\Antonio <strafaceant@gmail.com>
2018-01-28 14:06:30 +0100

    for the future

---
+++ eserciziduranteilcorso/solid/risolvi.java
@@ -0,0 +1,23 @@
+package eserciziduranteilcorso.solid;
+
+public class risolvi {
+
+	
+	public double Risolvi(String figura,int base,int basedue,int altezza) {
+		
+		double area = 0;
+		
+		if(figura.compareTo("rettangolo") == 0) {
+			area = base * altezza;
+		}
+		if(figura.compareTo("triangolo") == 0) {
+			area = (base * altezza)/2;
+		}
+		if(figura.compareTo("trapezio") == 0) {
+			area = ((base + base) * altezza)/2;
+		}
+		
+		
+		return area;
+	}
+}
