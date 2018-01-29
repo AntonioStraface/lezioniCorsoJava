15d76fcadae2bf8786f75ba6cae026f5a63cca5b
NOTB726\admin <gruppoclassees@gmail.com>
2018-01-29 12:33:07 +0100

    esercizio vocali francesco

---
+++ eserciziduranteilcorso/finemodulo1/Main.java
@@ -0,0 +1,11 @@
+package eserciziduranteilcorso.finemodulo1;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		String caratteri = "AEIOUaeiou";
+		char [] arrayVocali = {'C','i','i','a','o','!'};
+		Vocale.vocale(arrayVocali, caratteri);
+	}
+}
