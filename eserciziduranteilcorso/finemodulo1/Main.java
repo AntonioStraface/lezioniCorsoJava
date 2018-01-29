d5fcf6b717cb4361e8d6649bf1f2f2448fac2446
Ciobanu C <cristi.ciobanu@scai-consulting.grupposcai.it>
2018-01-29 12:37:34 +0100

    aggiornamento es vocali

--- eserciziduranteilcorso/finemodulo1/Main.java
+++ eserciziduranteilcorso/finemodulo1/Main.java
@@ -4,8 +4,15 @@ public class Main {
 
 	public static void main(String[] args)
 	{
-		String caratteri = "AEIOUaeiou";
-		char [] arrayVocali = {'C','i','i','a','o','!'};
-		Vocale.vocale(arrayVocali, caratteri);
+//		String caratteri = "AEIOUaeiou";
+//		char [] arrayVocali = {'C','i','i','a','o','!'};
+//		Vocale.vocale(arrayVocali, caratteri);
+		
+		String prova = "aoiueo";
+		char [] caratteri = new char [] {
+				'a', 'o', 'e', 'i', 'u'
+		};
+		
+		System.out.println(Motodi.mostCaratteri(prova, caratteri));
 	}
 }
